87e6afdc58a23a4cbbdcf3bea0f38c4c12227d3b
Shubham <shubham.dhingra@siemens.com>
2019-02-23 16:27:06 +0530

    Test Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  name = '';
+  public tenantList: any[] = [];
+  selectedName: boolean;
+  check = []
+  demoChk = [];
+  ngOnInit(): void {
+    this.getList();
+  }
+
+  getList() {
+    for (let i = 0; i < 12; i++) {
+      this.tenantList.push({
+        value: i,
+        name: ('tenant' + [i]),
+        id: ('id-' + i)
+
+      });
+    }
+  }
+  onSelected(name) {
+    console.log('event');
+    this.selectedName = (name);
+    console.log(this.selectedName);
+  }
+  toggleSelect(value, event) {
+    if (event.target.checked) {
+      this.demoChk.push(value);
+    }
+    else if (!event.target.checked) {
+      const indexx = this.demoChk.indexOf(value);
+      this.demoChk.splice(indexx, 1);
+    }
+    console.log(this.demoChk)
+  }
 }
